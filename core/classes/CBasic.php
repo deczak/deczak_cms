a411eb7a9783be097a200c0ccf012aec5cf2b13f
europa <europa@europa.intranet>
2020-08-26 19:54:31 +0200

    removed comments

--- core/classes/CBasic.php
+++ core/classes/CBasic.php
@@ -9,26 +9,5 @@ class	CBasic
 	{
 		$this -> m_aStorage		= [];
 	}
-/*
-	public function
-	getJSON(array $_jsonData)
-	{
-		return json_encode( $_jsonData , JSON_FORCE_OBJECT|JSON_HEX_APOS|JSON_HEX_QUOT );
-	}
-
-	public function
-	printJSON(array $_jsonData )
-	{
-		header('Content-Type: application/json');
-		echo json_encode( $_jsonData , JSON_FORCE_OBJECT|JSON_HEX_APOS|JSON_HEX_QUOT );
-		exit();
-	}	
-
-	public function
-	dbug(bool $_returnAsString = false)
-	{
-		tk::dbug($this -> m_aStorage, $_returnAsString);
-	}
-*/
 }
 ?>
