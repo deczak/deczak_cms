52b478182c761eaa35b1cee47b94b75e30c5fd2f
europa <europa@europa.intranet>
2020-12-02 20:35:23 +0100

    required sheme file

---
+++ core/shemes/shemeBackendPageObjectSimple.php
@@ -0,0 +1,13 @@
+<?php
+
+class shemeBackendPageObjectSimple extends shemeSimple
+{
+	public function
+	__construct()
+	{
+		parent::__construct();
+		$this -> setTableName('tb_backend_page_object_simple');
+	}
+}
+
+?>
