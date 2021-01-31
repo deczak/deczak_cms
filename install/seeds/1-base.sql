6650b8208b9a9325e0446015dcf0ab20a5c2ca22
europa <europa@europa.intranet>
2021-01-31 11:02:03 +0100

    install seeds

---
+++ install/seeds/1-base.sql
@@ -0,0 +1,32 @@
+INSERT INTO `tb_backend_page_path` (`node_id`, `page_id`, `page_language`, `page_path`, `node_rgt`, `node_lft`) VALUES
+(1, 0, '0', '', 4, 1),
+(2, 1, 'en', '/', 3, 2);
+
+INSERT INTO `tb_page_path` (`node_id`, `page_id`, `page_language`, `page_path`, `node_rgt`, `node_lft`) VALUES
+(1, 0, '0', '', 6, 1),
+(2, 1, 'en', '/', 3, 2),
+(3, 1, 'de', '/', 5, 4);
+
+INSERT INTO `tb_backend_menu` (`menu_group`, `menu_name`, `menu_icon`, `menu_order_by`) VALUES
+(1, 'PLACEHOLDER', '', 1),
+(2, 'PLACEHOLDER', '', 2),
+(3, 'PLACEHOLDER', '&#xf013;', 3);
+
+INSERT INTO `tb_languages` (`data_id`, `lang_key`, `lang_name`, `lang_name_native`, `lang_hidden`, `lang_locked`, `lang_default`, `lang_frontend`, `lang_backend`, `create_time`, `create_by`, `update_time`, `update_by`) VALUES
+(1, 'en', 'English', 'English', 0, 0, 1, 1, 1, %TIMESTAMP%, 1, 0, 0),
+(2, 'de', 'German', 'Deutsch', 1, 0, 0, 1, 1, %TIMESTAMP%, 1, 0, 0);
+
+INSERT INTO `tb_login_objects` (`data_id`, `object_id`, `object_databases`, `object_fields`, `object_session_ext`, `object_description`, `is_disabled`, `is_protected`, `create_time`, `create_by`, `update_time`, `update_by`) VALUES
+(12, 'ABKND', '["primary"]', '[{"name":"login_name","data_prc":"crypt","type":"text","is_username":"0","query_type":"compare","table":"tb_users_backend"},{"name":"login_pass","data_prc":"hash","type":"password","is_username":"1","query_type":"compare","table":"tb_users_backend"}]', '{"1":{"name":"user_name_last","data_prc":"crypt","table":"tb_users_backend","query_type":"compare"},"2":{"name":"language","data_prc":"text","table":"tb_users_backend","query_type":"compare"}}', 'Backend Access', 0, 1, %TIMESTAMP%, 1, 0, 0);
+
+INSERT INTO `tb_right_groups` (`group_id`, `group_name`, `group_rights`, `create_time`, `create_by`, `update_time`, `update_by`) VALUES
+(1, 'administrator', '{"1":["view","create","edit","delete"],"2":["index","view","create","edit","delete"],"3":["index","view","create","edit","delete"],"4":["view","create","edit","delete"],"5":["view","create","edit","delete"],"6":["index","view","create","edit","delete"],"7":["view","create","edit","delete"],"8":["index","view","create","edit","delete"],"9":["index","view","create","edit","delete"],"10":["index","view","create","edit","delete"],"11":["index","view","edit","delete"],"12":["index","view","create","edit","delete"],"13":["index","view","create","edit","delete"],"14":["index","edit"],"15":["index","view","create","edit","delete"],"16":["index","view","create","edit","delete"],"17":["index","view","create","edit","delete"],"18":["index","view","edit"]}', %TIMESTAMP%, 1, 0, 0);
+
+INSERT INTO `tb_users_backend` (`data_id`, `login_name`, `login_pass`, `login_count`, `user_id`, `user_name_first`, `user_name_last`, `user_mail`, `time_login`, `create_time`, `update_time`, `cookie_id`, `recover_key`, `recover_timeout`, `is_locked`,`language`) VALUES
+(1, '%USER_NAME%', '%USER_PASSWORD%', 0, '1', '%USER_FIRST_NAME%', '%USER_LAST_NAME%', '%USER_MAIL%', 0, %TIMESTAMP%, 0, '{}', 0, 0, 0,'en');
+
+INSERT INTO `tb_users_groups` (`data_id`, `user_id`, `group_id`) VALUES
+(1, '1', 1);
+
+INSERT INTO `tb_users_register` (`data_id`, `user_id`, `user_type`, `user_hash`, `user_name`) VALUES
+(1, '1', 0, NULL, NULL);
---
+++ install/seeds/2-example.sql
@@ -0,0 +1,19 @@
+INSERT INTO `tb_page` (`data_id`, `node_id`, `page_id`, `page_version`, `page_template`, `update_reason`, `hidden_state`, `cache_disabled`, `crawler_index`, `crawler_follow`, `menu_follow`, `publish_from`, `publish_until`, `publish_expired`, `page_auth`, `create_time`, `create_by`, `update_time`, `update_by`) VALUES
+(1, 2, 1, 1, 'default', '', 0, 0, 1, 1, 1, 0, 0, 0, NULL, %TIMESTAMP%, 1, 0, 0),
+(2, 3, 1, 1, 'default', '', 0, 0, 1, 1, 1, 0, 0, 0, '0', %TIMESTAMP%, 1, 0, 0);
+
+INSERT INTO `tb_page_header` (`data_id`, `node_id`, `page_id`, `page_language`, `page_title`, `page_name`, `page_description`, `page_version`) VALUES
+(1, 2, 1, 'en', 'Home', 'Home ', 'Home Description', 1),
+(2, 3, 1, 'de', 'Startseite', 'Startseite', 'Startseiten Beschreibung', 1);
+
+INSERT INTO `tb_page_object` (`object_id`, `node_id`, `page_version`, `module_id`, `content_id`, `object_order_by`, `update_reason`, `create_time`, `create_by`, `update_time`, `update_by`) VALUES
+(1, 2, 1, 4, 1, 2, '', %TIMESTAMP%, 1, 0, 0),
+(2, 2, 1, 5, 1, 1, '', %TIMESTAMP%, 1, 0, 0),
+(3, 3, 1, 5, 1, 1, '', %TIMESTAMP%, 1, 0, 0),
+(4, 3, 1, 4, 1, 2, '', %TIMESTAMP%, 1, 0, 0);
+
+INSERT INTO `tb_page_object_simple` (`data_id`, `object_id`, `body`, `params`) VALUES
+(1, 1, '<p>For more information and the documentation about the content management system see <a href="https://www.dennczak.de/en/projects/content-management-system/" style="">this website</a>&nbsp;(external link)</p>', ''),
+(2, 2, '<h1>Welcome to your new<p>Content Management System<br></p></h1>', ''),
+(3, 3, '<h1>Willkommen zu Ihrem neuen<p>Content Management System</p></h1>', ''),
+(4, 4, '<p>Für weitere Informationen und die Dokumentation zu diesem Content Management System schauen Sie auf <a href="https://www.dennczak.de/projekte/content-management-system/">diese Website</a> (externer Link)</p>', '');
