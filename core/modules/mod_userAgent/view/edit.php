45270b9b8e1a7dac825342807454f9d1a2a0fe58
europa <europa@europa.intranet>
2021-04-10 12:41:16 +0200

    Fix Module User Agent

--- core/modules/mod_userAgent/view/edit.php
+++ core/modules/mod_userAgent/view/edit.php
@@ -47,12 +47,12 @@ else
 					<div class="group-head width-100"><?php echo $language -> string('M_BEUSERAG_USERAGENT'); ?></div>
 
 					<div class="input width-25">
-						<label>IP <?php echo $language -> string('M_BEUSERAG_AGENTNAME'); ?></label>
+						<label><?php echo $language -> string('M_BEUSERAG_AGENTNAME'); ?></label>
 						<input type="text" name="agent_name" value="" maxlength="35">
 					</div>
 
 					<div class="input width-50">
-						<label>IP <?php echo $language -> string('M_BEUSERAG_AGENTSUFFIX'); ?></label>
+						<label><?php echo $language -> string('M_BEUSERAG_AGENTSUFFIX'); ?></label>
 						<input type="text" name="agent_suffix" value="" maxlength="75">
 					</div>
 			
