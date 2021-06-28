7723b43ffc56a60a1e1f9ff3815b8a3347066556
deczak <>
2021-06-28 19:19:37 +0200

    default template handling

--- install/page-templates/pt-default/fs/head.php
+++ install/page-templates/pt-default/fs/head.php
@@ -1,3 +1,3 @@
 <?php /* insert here stuff that comes inside the head-tag */ ?>
 
-<link href="<?php echo CMS_SERVER_URL; ?>css/default.css" rel="stylesheet" title="default" media="screen">
+<link href="<?php echo CMS_SERVER_URL . DIR_TEMPLATES; ?>default/css/default.css" rel="stylesheet" title="default" media="screen">
