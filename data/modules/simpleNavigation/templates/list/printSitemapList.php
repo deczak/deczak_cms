6e3ff8a3c2c839202a6ddf523562e18bcae11d4d
deczak <>
2022-01-13 06:38:30 +0100

    fix navigation modul

--- data/modules/simpleNavigation/templates/list/printSitemapList.php
+++ data/modules/simpleNavigation/templates/list/printSitemapList.php
@@ -14,8 +14,6 @@ function printSitemapList(array &$_sitemap, int $_sitemapIndex, int $_sitemapLev
 	$childIsActive 	= false;
 	for($i = $_sitemapIndex; $i < count($_sitemap); $i++)
 	{
-
-
 		if(	(		$_sitemap[$i] ->  listing_hidden == 1
 				&&	$_sitemap[$i] -> hidden_state === 2 
 				&& $_sitemap[$i] -> page_auth == 0
@@ -40,7 +38,10 @@ function printSitemapList(array &$_sitemap, int $_sitemapIndex, int $_sitemapLev
 	for($i = $_sitemapIndex; $i < count($_sitemap); $i++)
 	{
 		if($_sitemapLevel !== $_sitemap[$i] -> level && $_sitemap[$i] -> listing_type == 'subpages')
+		{
+			$i = $_sitemap[$_sitemapIndex - 1] -> offspring + $_sitemapIndex - 1;
 			break;
+		}
 
 		if(	(		$_sitemap[$i] -> listing_hidden == 1
 				&&	$_sitemap[$i] -> hidden_state === 2 
@@ -59,23 +60,17 @@ function printSitemapList(array &$_sitemap, int $_sitemapIndex, int $_sitemapLev
 
 		$issActive = in_array($_sitemap[$i] -> node_id, array_column($_pagePath, 'nodeId'));
 
-
 		if(CMS_BACKEND)
 			echo '<li class="'. ($issActive ? 'active-page' : '') .'"><a data-node="'. $_sitemap[$i] -> node_id .'" class="darkblue" href="'. CMS_SERVER_URL_BACKEND .'pages/view/'. $_sitemap[$i] -> page_language .'/'. $_sitemap[$i] -> node_id .'" title="'. $_sitemap[$i] -> page_title .'">'. $_sitemap[$i] -> page_name .'</a>';
 		else
 			echo '<li class="'. ($issActive ? 'active-page' : '') .'"><a data-node="'. $_sitemap[$i] -> node_id .'" class="darkblue" href="'. CMS_SERVER_URL . URL_LANG_PRREFIX . substr($_sitemap[$i] -> page_path, 1) .'" title="'. $_sitemap[$i] -> page_title .'" '. ($_sitemap[$i] -> menu_follow == 0 ? 'rel="nofollow"' : '') .'>'. $_sitemap[$i] -> page_name  .'</a>';
 
-
-
-
 		if($_sitemap[$i] -> offspring != 0)
+		{
 			$i = printSitemapList($_sitemap, ($i + 1), ($_sitemapLevel + 1), $_objectParams, $_pagePath, $_printRoodNode);
-
-		echo '</li>';
-
-
+		}
 	}
 
 	echo '</ul>';
-	return $i - 1;
+	return $i;
 }
