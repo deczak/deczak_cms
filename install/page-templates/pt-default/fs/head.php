5b8e56ddabd3d316c2ad392e0cd4ae5910ca0cee
deczak <>
2022-12-22 09:49:33 +0100

    install default template

--- install/page-templates/pt-default/fs/head.php
+++ install/page-templates/pt-default/fs/head.php
@@ -2,5 +2,5 @@
 
 <link href="<?php echo CMS_SERVER_URL . DIR_TEMPLATES; ?>default/css/default.css" rel="stylesheet" title="default" media="screen">
 
-<script src="<?php echo CMS_SERVER_URL; ?>js/cms-xhr.js"></script>
-<script src="<?php echo CMS_SERVER_URL; ?>js/cms-toolkit.js"></script>
+<script src="<?php echo CMS_SERVER_URL; ?>js/cms-xhr.php"></script>
+<script src="<?php echo CMS_SERVER_URL; ?>js/cms-toolkit.php"></script>
